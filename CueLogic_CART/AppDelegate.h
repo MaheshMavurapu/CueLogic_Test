2191df7774f6e78f7cc9fbf90c07f76e4bb10a03
Mahesh Mavurapu <MaheshMavurapu@maheshs-macbook-pro.local>
2016-09-20 18:56:35 +0530

    Initial Commit

---
+++ CueLogic_CART/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CueLogic_CART
+//
+//  Created by Mahesh Mavurapu on 20/09/16.
+//  Copyright © 2016 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
